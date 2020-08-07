b4d704e6c90769ac50b08308ac652fd0b3e7db19
NicholasLKSharp <nicholas.lkading@gmail.com>
2020-08-07 22:55:10 +0200

    updated message size

--- clientlib.py
+++ clientlib.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import math
 
 serverip = 'localhost'#'76.176.57.74'
 serverport = 8080
@@ -7,15 +8,17 @@ packetsize = 32
 
 def inputProcessing(op, input):
     inp = op + ':' + input
-    outlen = int(len(inp.encode('utf-8'))/packetsize)
-    outlen+=len(str(outlen))
-    return str(outlen) + ':' + inp
+    x = len(inp)+1
+    size = len(str(x))
+    base = pow(10, size)-size
+    result = math.ceil(x/base)+x
+    return str(result) + ':' + inp
 
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((serverip, serverport))
-outmessage = inputProcessing('repeat', 'this is sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssthe message')
+outmessage = inputProcessing('repeat', 'this issssssss the message')
 print('outmessage= '+ outmessage)
 s.send(outmessage.encode('utf-8'))
 
@@ -31,7 +34,7 @@ while True:
     print('packet (' + str(packetindex) + ')')
     if packetcount == -1:
         parts = msgdata.split(':')
-        packetcount = int(parts[0])
+        packetcount =  math.ceil(int(parts[0])/packetsize)
         residue = parts[1].encode('utf-8')
         indata+=residue
     else:
--- serverlib.py
+++ serverlib.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import threading
+import math
 
 serverport = 8080
 packetsize = 32
@@ -11,17 +12,21 @@ def serverProcessing(op, bytearr, addr):
         return bytearr
 
 def inputProcessing(input):
-    outlen = int(len(input.encode('utf-8'))/packetsize)
-    outlen+=len(str(outlen))
-    return str(outlen) + ':' + input
+    x = len(input)+1
+    size = len(str(x))
+    base = pow(10, size)-size
+    result = math.ceil(x/base)+x
+    return str(result) + ':' + input
 
 def serverwaiter():
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     s.bind(('',serverport))
     s.listen(10)
+    print('initialized server')
     while True:
         cobj,addr = s.accept()
+        print('client connected')
         mythread = threading.Thread(target=client,name='TCP client {}'.format(threading.active_count()-1),args=(cobj,addr))
         mythread.daemon = True # So client threads die on main thread exit.
         mythread.start()
@@ -39,10 +44,8 @@ def client(cobj,addr):
         msgdata = data.decode('utf-8')
         print('packet (' + str(packetindex) + ')')
         if packetcount == -1:
-            print(msgdata)
             parts = msgdata.split(':')
-            print(parts)
-            packetcount = int(parts[0])
+            packetcount = math.ceil(int(parts[0])/packetsize)
             operation = parts[1]
             residue = parts[2].encode('utf-8')
             indata+=residue
